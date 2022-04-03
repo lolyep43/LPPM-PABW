c5b680deb272a91a1e3a9ca48260f0791af1900c
IGustiFajar <73781057+IGustiFajar@users.noreply.github.com>
2022-04-03 23:37:29 +0800

    Delete User & FIX API for User

--- routes/api.php
+++ routes/api.php
@@ -19,16 +19,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+# API Portofolio 
 Route::get('lihat-portofolio', [API::class, 'PortofolioIndex']);
 Route::post('tambah-portofolio', [API::class, 'PortofolioStore']);
 Route::put('edit-portofolio/{id}',  [API::class, 'PortofolioEdit']);
 Route::delete('hapus-portofolio/{id}',  [API::class, 'PortofolioDelete']);
 
+# API Anggota
 Route::get('lihat-anggota', [API::class, 'AnggotaIndex']);
 Route::post('tambah-anggota', [API::class, 'AnggotaStore']);
 Route::put('edit-anggota/{id}',  [API::class, 'AnggotaEdit']);
 Route::delete('hapus-anggota/{id}',  [API::class, 'AnggotaDelete']);
 
+# API Users
 Route::get('lihat-user', [API::class, 'UserIndex']);
 Route::post('tambah-user', [API::class, 'UserStore']);
 Route::put('edit-user/{id}',  [API::class, 'UserEdit']);
